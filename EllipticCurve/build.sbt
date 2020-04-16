fdd35bb2589755387576dd9bfc161518d7f4664e
massimo-a <43350227+massimo-a@users.noreply.github.com>
2020-04-16 17:02:31 -0400

    Uploaded project

---
+++ EllipticCurve/build.sbt
@@ -0,0 +1,5 @@
+name := "EllipticCurve"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ EllipticCurve/src/main/scala/ModInt.scala
@@ -0,0 +1,46 @@
+case class ModInt(n: BigInt, mod: BigInt) {
+  def +(m: BigInt): ModInt = {
+    ModInt((n+m).mod(mod), mod)
+  }
+
+  def -(m: BigInt): ModInt = {
+    ModInt((n-m).mod(mod), mod)
+  }
+
+  def *(m: BigInt): ModInt = {
+    ModInt((n*m).mod(mod), mod)
+  }
+
+  def **(m: BigInt): ModInt = {
+    if(m <= 0) {
+      val a = n.modPow(-m, mod)
+      ModInt(a, mod).invModPrime()
+    } else {
+      ModInt(n.modPow(m, mod), mod)
+    }
+  }
+
+  def invModPrime(): ModInt = {
+    ModInt(n.modPow(mod-2, mod), mod)
+  }
+
+  def /(m: BigInt): ModInt = {
+    if(m.gcd(mod) != BigInt(1)) {
+      throw new Exception(f"""In order to divide $n by $m modulo $mod the GCD of $m and $mod has to equal 1, but it actually equals ${m.gcd(mod)}""")
+    }
+    val a = ModInt(m, mod).invModPrime()
+    ModInt((n*a.n).mod(mod), mod)
+  }
+}
+
+
+case object ModInt {
+  implicit def int2bigint(i: Int): BigInt = {
+    BigInt(i)
+  }
+  def apply(m: Int): Int => ModInt = {
+    n: Int => {
+      ModInt(n, m)
+    }
+  }
+}
